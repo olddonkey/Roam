fd3d4e6153dc450cd5a41d91db3d5d49f98c68e9
olddonkey <olddonkeyblog@gmail.com>
2018-03-21 22:05:29 -0500

    Add IdentityManager

---
+++ Roam/Business Logic/IdentityManager.swift
@@ -0,0 +1,22 @@
+//
+//  IdentityManager.swift
+//  Roam
+//
+//  Created by olddonkey on 2018/3/21.
+//  Copyright © 2018年 olddonkey. All rights reserved.
+//
+
+import Cocoa
+
+class IdentityManager: NSObject {
+    
+    func appDidLaunched() {
+        
+    }
+    
+    func isAccessTokenValid() -> Bool {
+        return true
+    }
+    
+    
+}
---
+++ Roam/Business Logic/TokenVerification/AccessTokenVerifier.swift
@@ -0,0 +1,25 @@
+//
+//  AccessTokenVerifier.swift
+//  Roam
+//
+//  Created by olddonkey on 2018/3/21.
+//  Copyright © 2018年 olddonkey. All rights reserved.
+//
+
+import Cocoa
+
+class AccessTokenVerifier: NSObject {
+    // 1. Token is not in Keychain
+    // 2. Token is in Keycain, but it is invalid
+    // 3. 
+    var userToken: UserToken
+    
+    init(userToken: UserToken) {
+        self.userToken = userToken
+    }
+    
+    func isTokenValid() -> Bool {
+        
+        return true
+    }
+}
